db6c3a12184d0b53e43ddaa3ac70576107b65860
Pavel Stepanov <Pavel@PL-ASUS>
2018-09-30 12:14:31 +0300

    modified utilities

--- util/kp.js
+++ util/kp.js
@@ -2,6 +2,15 @@ const axios = require('axios');
 const moment = require('moment');
 const kpUrl = 'http://services.swpc.noaa.gov/text/daily-geomagnetic-indices.txt';
 
+class KpIndex{
+   
+    constructor(val, startPeriod){
+        const periodLength = 3;
+        this.value = val;
+        this.startPeriod = startPeriod;
+        this.endPeriod = this.startPeriod + periodLength;
+    }
+} 
 
 class KpIndexData {
     constructor(strName){
@@ -10,24 +19,50 @@ class KpIndexData {
     }
 }
 
-function ExtractKpIndices(strData, offset, name){
-    const ixCount = 8;
-    let kpData = new KpIndexData(name);
-    for(let i=offset; i < offset + 2*ixCount; i+=2){
-        let kpIndex = Number.parseInt(strData[i] + strData[i+1]);
-        kpData.indices.push(kpIndex);
+class KpIndexParser{
+    constructor(strData){
+        this.strData = strData;
+        this.arrLines = strData.split('\n');
     }
-    return kpData;
+
+    extractKpIndices(strData, offset, name){
+        const ixCount = 8;
+        const periods = [0,3,6,9,12,15,18,21];
+        let kpData = new KpIndexData(name);
+        let index = 0;
+        for(let i=offset; i < offset + 2*ixCount; i+=2){
+            let kpIndexValue = Number.parseInt(strData[i] + strData[i+1]);
+            let periodStart = periods[index++];
+            kpData.indices.push(new KpIndex(kpIndexValue,periodStart));
+        }
+        return kpData;
+    }
+
+    parseKpDataLine(strData){
+        let result = [];
+        const startOffset = 17;
+        const dataBlockLength = 23
+
+        result.push(this.extractKpIndices(strData, startOffset, "Middle Latitude"));
+        result.push(this.extractKpIndices(strData, startOffset + dataBlockLength, "High Latitude"));
+        result.push(this.extractKpIndices(strData, startOffset + 2*dataBlockLength, "Estimated"));
+        return result;    
+    }
+
+    parseDate(date) {
+        let strDate = moment(date).format('YYYY MM DD');
+        console.log(`Reading Kp index for date: '${strDate}'`);
+        
+        for(let ix=0; ix < this.arrLines.length; ix++){
+            if(this.arrLines[ix].startsWith(strDate)){
+                var parsed = this.parseKpDataLine(this.arrLines[ix]);
+                return parsed;
+            }
+        }
+        return null; // date not found    
+    }
 }
 
-function ParseKpDataLine(strData){
-    let result = [];
-
-    result.push(ExtractKpIndices(strData, 17, "Middle Latitude"));
-    result.push(ExtractKpIndices(strData, 17 + 23, "High Latitude"));
-    result.push(ExtractKpIndices(strData, 17 + 46, "Estimated"));
-    return result;
-}
 
 function DisplayKpIndex(kpDataArray){
     console.log("Displaying KpIndex...")
@@ -46,32 +81,28 @@ function DisplayKpIndex(kpDataArray){
         let kpData = kpDataArray[ix];
         console.log(`\n${kpData.name}`)
         for(let i=0; i < kpData.indices.length; i++){
-            console.log(`Kp index [${periods[i]}] = ${kpData.indices[i]}`);
+            let periodStart = `${kpData.indices[i].startPeriod.toString().padStart(2,'0')}:00`;
+            let periodEnd = `${kpData.indices[i].endPeriod.toString().padStart(2,'0')}:00`;
+            console.log(`Kp index [${periodStart}-${periodEnd}] = ${kpData.indices[i].value}`);
         }
             
     }
 }
 
-function ParseKpData(arrLines) {
-    let strDate = moment().format('YYYY MM DD');
-    console.log(`Reading Kp index for today: '${strDate}'`);
-    
-    for(let ix=0; ix < arrLines.length; ix++){
-        if(arrLines[ix].startsWith(strDate)){
-            var parsed = ParseKpDataLine(arrLines[ix]);
-            DisplayKpIndex(parsed);
-        }
-    }
-}
 
 function DownloadKpData(callback){
     axios.get(kpUrl)
     .then(response => {
         console.log("Data retrieved success");
-        let lines = response.data.toString().split('\n');
+        //let lines = response.data.toString().split('\n');
         if (callback)
-            callback(lines);
+            callback(response.data.toString());
     });
 }
 
-DownloadKpData(ParseKpData);
+DownloadKpData((strData) => {
+    let parser = new KpIndexParser(strData);
+    let date = new Date();
+    let kpDataArray = parser.parseDate(date);
+    DisplayKpIndex(kpDataArray);
+});
